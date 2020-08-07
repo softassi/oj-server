ba35d57274cccf0a2ad14cd7df6ef4d20eb4974a
cunkoulaocai <1184016190@qq.com>
2020-08-07 23:24:20 +0800

    1 添加pagedto 2 修改一些错误

--- src/main/java/com/softassi/oj/server/controller/admin/ExerciseController.java
+++ src/main/java/com/softassi/oj/server/controller/admin/ExerciseController.java
@@ -1,14 +1,20 @@
 package com.softassi.oj.server.controller.admin;
 
+import com.softassi.oj.server.dto.ExerciseDto;
+import com.softassi.oj.server.dto.PageDto;
 import com.softassi.oj.server.dto.ResultBody;
+import com.softassi.oj.server.object.Exercise;
+import com.softassi.oj.server.service.ExerciseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @ClassName : TagController
@@ -20,19 +26,34 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "题目管理接口")
 @RequestMapping("/exercise")
 public class ExerciseController {
+    private static final Logger LOG = LoggerFactory.getLogger(ExerciseController.class);
+
+    @Autowired
+    private ExerciseService exerciseService;
 
     @RequestMapping("/save")
-    @ApiOperation("保存一个标签")
-    public ResultBody save() {
-        return ResultBody.success();
+    @ApiOperation("添加一个习题")
+    public ResultBody save(@RequestBody ExerciseDto exerciseDto) {
+        Exercise save = exerciseService.save(exerciseDto);
+        return ResultBody.success(save);
     }
 
     @GetMapping(value = "/{id}")
-    @ApiOperation("得到一个标签")
+    @ApiOperation("得到一个习题")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", value = "题目id", paramType = "path")
     })
     public ResultBody get(@PathVariable("id") String id) {
         return ResultBody.success();
     }
+
+    @GetMapping("/list")
+    public ResultBody list(@RequestBody PageDto pageDto) {
+        LOG.info("page:{}, size:{}", pageDto.getPage(), pageDto.getSize());
+        List<Exercise> list = exerciseService.list(pageDto);
+        return ResultBody.success(list);
+    }
+    
+
 }
+
--- src/main/java/com/softassi/oj/server/service/ExerciseService.java
+++ src/main/java/com/softassi/oj/server/service/ExerciseService.java
@@ -1,7 +1,10 @@
 package com.softassi.oj.server.service;
 
+import com.softassi.oj.server.dto.ExerciseDto;
+import com.softassi.oj.server.dto.PageDto;
 import com.softassi.oj.server.object.Exercise;
 import com.softassi.oj.server.repository.ExerciseRepository;
+import com.softassi.oj.server.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,18 +20,25 @@ import java.util.Optional;
  * @Date: 2020-07-27 20:58
  */
 @Service
-public class ExerviseService {
+public class ExerciseService {
 
     @Autowired
     private ExerciseRepository exerciseRepository;
+
+    public Exercise save(ExerciseDto exerciseDto) {
+        Exercise copy = CopyUtil.copy(exerciseDto, Exercise.class);
+        Exercise result = exerciseRepository.save(copy);
+        return result;
+    }
 
     public Exercise getById(String id) {
         Optional<Exercise> byId = exerciseRepository.findById(id);
         return byId.orElse(null);
     }
 
-    public List<Exercise> list(PageRequest pageRequest) {
-        Page<Exercise> exercisePage = exerciseRepository.findAll(pageRequest);
+    public List<Exercise> list(PageDto pageDto) {
+        PageRequest of = PageRequest.of(pageDto.getPage(), pageDto.getSize());
+        Page<Exercise> exercisePage = exerciseRepository.findAll(of);
         List<Exercise> content = exercisePage.getContent();
         return content;
     }
@@ -40,6 +50,4 @@ public class ExerviseService {
     public void submit(Exercise exercise) {
 
     }
-    
-
 }
